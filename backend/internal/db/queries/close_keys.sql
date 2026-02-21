f5c774e8c3296815d88b354ef84502b065ca254d
megu <nhrmgm1223@gmail.com>
2026-02-21 14:33:28 +0900

    db: remove legacy close_keys query

--- backend/internal/db/queries/close_keys.sql
+++
@@ -1,4 +0,0 @@
--- name: InsertCloseExecution :execrows
-INSERT INTO close_executions (team_id, scope, target_date, task_id, created_at)
-VALUES ($1, $2, $3, NULLIF($4, '')::uuid, NOW())
-ON CONFLICT (team_id, scope, target_date, dedupe_task_key) DO NOTHING;
