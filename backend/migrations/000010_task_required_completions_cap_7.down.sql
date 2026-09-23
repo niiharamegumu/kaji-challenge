e14be2d8536bfd1bf3ea015acb2769c8ebeb89cd
megu <nhrmgm1223@gmail.com>
2026-09-23 21:14:25 +0900

    chore: 旧PostgreSQL migrationを撤去する（2/5）

--- backend/migrations/000010_task_required_completions_cap_7.down.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE tasks
-  DROP CONSTRAINT IF EXISTS tasks_required_completions_per_week_check;
-
-ALTER TABLE tasks
-  ADD CONSTRAINT tasks_required_completions_per_week_check
-  CHECK (
-    required_completions_per_week >= 1
-  );
