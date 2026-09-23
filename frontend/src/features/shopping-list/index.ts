347e337b3d370e2451fcf36551227e8a01b69d91
megu <nhrmgm1223@gmail.com>
2026-09-23 21:16:12 +0900

    chore: 旧frontendのshopping-list実装を撤去する

--- frontend/src/features/shopping-list/index.ts
+++
@@ -1,8 +0,0 @@
-export {
-  type ShoppingItemFormState,
-  ShoppingListItemsSection,
-} from "./components/ShoppingListManager";
-export {
-  useShoppingItemMutations,
-  useShoppingItemsQuery,
-} from "./hooks/useShoppingList";
