111a63ec00300432912f7aee0cc7bd551dd81b99
megu <nhrmgm1223@gmail.com>
2026-09-23 21:16:06 +0900

    chore: 旧frontendのshell実装を撤去する

--- frontend/src/features/shell/api/teamMembersApi.ts
+++
@@ -1,5 +0,0 @@
-import { getTeamCurrentMembers } from "../../../lib/api/generated/client";
-
-export async function listCurrentTeamMembers() {
-  return (await getTeamCurrentMembers()).data.items;
-}
